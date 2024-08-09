d25f7e0f16052e62052583b632f16334441fc89a
Tran Phan Vinh Sang <vinhsang7410@gmail.com>
2024-08-10 04:43:49 +0700

    update ami

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -12,11 +12,11 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = var.region
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-066e0ce59fb6af699"
   instance_type = "t2.micro"
 
   tags = {
