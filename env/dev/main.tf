06bcfaf3bf2d1b73f3097dc35901cd806403d038
HuynhHuuThang <21521771@gm.uit.edu.vn>
2024-08-10 05:21:12 +0700

    update

---
+++ env/dev/main.tf
@@ -0,0 +1,25 @@
+////https://www.microfocus.com/documentation/arcsight/arcsight-platform-22.1/arcsight-admin-guide-22.1/Content/deployment_cloud/AWS_CLI_configure.htm
+////
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0497a974f8d5dcef8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
---
+++ env/dev/variables.tf
@@ -0,0 +1,4 @@
+variable "region" {
+  description = "region of aws"
+  default = "ap-southeast-1"
+}
