993105f6ac1e8febd3320fa7339b531f33ee0ed5
Tran Phan Vinh Sang <vinhsang7410@gmail.com>
2024-08-11 05:29:02 +0700

    fix ci

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,15 +1,15 @@
 ////https://www.microfocus.com/documentation/arcsight/arcsight-platform-22.1/arcsight-admin-guide-22.1/Content/deployment_cloud/AWS_CLI_configure.htm
 ////
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.16"
+#     }
+#   }
 
-  required_version = ">= 1.2.0"
-}
+#   required_version = ">= 1.2.0"
+# }
 
 provider "aws" {
   region  = var.region
